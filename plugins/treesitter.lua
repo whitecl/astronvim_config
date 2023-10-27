619c6b931f92c0a120c7cc23ef6a6c863c943a55
Chris White <chris.white@thoughtbot.com>
2023-10-27 14:58:49 -0600

    add default treesitter languages

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = { "elixir", "lua", "javascript", "ruby", "typescript"  },
   },
 }
